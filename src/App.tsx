b8a38c45f5bdeb185fa0abca8e53e3859968427c
Teofilus Robert <teofilusrobert@gmail.com>
2025-07-21 14:55:21 +0700

    Add Interactive Hero

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Hero from './components/Hero';
 
 const App: React.FC = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-grow">
+                <Hero />
                 <About />
                 <Projects />
                 <Contact />
